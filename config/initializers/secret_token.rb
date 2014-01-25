c5a69cb440393f91b422c5ad26de6a9354d744fa
Chu Anh Tuan <chu.anh.tuan@framgia.com>
2014-01-25 11:27:55 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fet::Application.config.secret_key_base = '6e8336f244f3d1e5bd23c8383ad0bf332d19019e9b8953f4fbc31db2436083841d702e39ad5b4a121cb80bd98dff64061e2e0e2bba2082d3a7ac318d1edd6679'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fet::Application.config.session_store :cookie_store, key: '_fet_session'
